f38ceab9fbfc7a82f71e560b99249ff3e66b5611
Thrisith <tthrisith@gmai.com>
2025-05-12 21:42:58 +0530

    Second Push - Basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { HelloWorldComponent } from './hello-world/hello-world.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/hello-world/hello-world.component.ts
+++ src/app/hello-world/hello-world.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { retry } from 'rxjs';
 
 @Component({
   selector: 'app-hello-world',
@@ -7,5 +8,36 @@ import { Component } from '@angular/core';
   styleUrl: './hello-world.component.css'
 })
 export class HelloWorldComponent {
-  title= "Hello Thrisith";
+  
+  title = "Movies";
+  movies = [{"name":"bulbasaur","url":"https://pokeapi.co/api/v2/pokemon/1/"},{"name":"ivysaur","url":"https://pokeapi.co/api/v2/pokemon/2/"},{"name":"venusaur","url":"https://pokeapi.co/api/v2/pokemon/3/"},{"name":"charmander","url":"https://pokeapi.co/api/v2/pokemon/4/"},{"name":"charmeleon","url":"https://pokeapi.co/api/v2/pokemon/5/"},{"name":"charizard","url":"https://pokeapi.co/api/v2/pokemon/6/"},{"name":"squirtle","url":"https://pokeapi.co/api/v2/pokemon/7/"},{"name":"wartortle","url":"https://pokeapi.co/api/v2/pokemon/8/"},{"name":"blastoise","url":"https://pokeapi.co/api/v2/pokemon/9/"},{"name":"caterpie","url":"https://pokeapi.co/api/v2/pokemon/10/"},{"name":"metapod","url":"https://pokeapi.co/api/v2/pokemon/11/"},{"name":"butterfree","url":"https://pokeapi.co/api/v2/pokemon/12/"},{"name":"weedle","url":"https://pokeapi.co/api/v2/pokemon/13/"},{"name":"kakuna","url":"https://pokeapi.co/api/v2/pokemon/14/"},{"name":"beedrill","url":"https://pokeapi.co/api/v2/pokemon/15/"},{"name":"pidgey","url":"https://pokeapi.co/api/v2/pokemon/16/"},{"name":"pidgeotto","url":"https://pokeapi.co/api/v2/pokemon/17/"},{"name":"pidgeot","url":"https://pokeapi.co/api/v2/pokemon/18/"},{"name":"rattata","url":"https://pokeapi.co/api/v2/pokemon/19/"},{"name":"raticate","url":"https://pokeapi.co/api/v2/pokemon/20/"}];
+
+
+
+  // value = "Ghost";
+  // value2= this.value;
+  // handleInput(event:any){
+  //   console.log(event);
+  //   this.value = (event.target as HTMLInputElement).value;
+  // }
+
+
+  // count = 0;
+  // clickMe(){
+  //   this.count++;
+  // }
+
+
+
+  // getName(){
+  //   return "Thrisith"
+  // }
+  // isDisabled(val:boolean)
+  // {
+  //   return val;
+  // }
+  // color = "red";
+  // getMax(f:number,s:number){
+  //   return Math.max(f,s);
+  // }
 }
